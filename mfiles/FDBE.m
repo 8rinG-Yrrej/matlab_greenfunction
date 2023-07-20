13fbd789761e63881d68f05ba9c444f163d34a3c
G,Y <5068630+8rinG-Yrrej@users.noreply.github.com>
2023-07-20 11:27:08 -0400

    Push.

--- mfiles/FDBE.m
+++ mfiles/FDBE.m
@@ -1,6 +1,10 @@
 function [f,nB,fPnB,fMf,dfomfun,dnBomfun]=FDBE
-%FDBE: collection of Fermi-Dirac and Einstein-Boltzmann distributions
-% [f,nB,fPnB,fMf,dfomfun,dnBomfun]=FDBE;
+%FDBE: collection of Fermi-Dirac and Einstein-Boltzmann distributions. Computed in a even-odd symmetric way in frequency, and avoids divergence at zero.
+% [f,nB,fPnB,fMf,dfom,dnBom]=FDBE;
+% f(omega/T) is fermi-dirac. dfom its temperature derivative.
+% nB(omega/T) is bose-einstein. nB(0)=0; dnBom its temperature derivative. Relies on if_then_else from matlab_utility.
+% fPnB(x,y)=n_f(x/T)+n_B(y/T)
+% fMf(x,y)=f(x/T)-f(y/T)
 
 f=str2func('@(xoT) (1-tanh(xoT/2))/2'); 
 nB=str2func('@(xoT) if_then_else(xoT==0, 0, (coth(xoT/2)-1)/2)');
